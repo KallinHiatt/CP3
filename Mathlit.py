11f895f2acf0021252d5c80600d293a022e6a0ba
KallinHiatt <kallin.hiatt@ucas-edu.net>
2024-09-09 11:40:57 -0600

    I created a code that asks what the user's name is, and returns a print statement that asks how they are doing.

---
+++ Mathlit.py
@@ -0,0 +1,10 @@
+print("Hey, wassup.")
+print("What's your name?")
+name = input(" ")
+print("Hello", name + ", how are you doing today?")
+r = input("")
+bad = dict{
+    "not good": 1
+    "bad" : 1
+    "not great"
+}

9b2720d680335fb95f10d0527da63cad199cd8a1
KallinHiatt <kallin.hiatt@ucas-edu.net>
2024-09-10 11:01:49 -0600

    #This is about how your day was

--- Mathlit.py
+++ Mathlit.py
@@ -3,8 +3,4 @@ print("What's your name?")
 name = input(" ")
 print("Hello", name + ", how are you doing today?")
 r = input("")
-bad = dict{
-    "not good": 1
-    "bad" : 1
-    "not great"
-}
+bad = "The dog."
